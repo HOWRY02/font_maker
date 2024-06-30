8b5493d71706876f6704adf3fe4425c45edd358c
Phuc <huynhvinhphuc101@gmail.com>
2024-06-30 11:54:21 +0700

    add function run multiple files

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
+import re
 import shutil
 import tempfile
 import tkinter
 from tkinter import filedialog
 
-from src.handwrite.sheettopng import SHEETtoPNG
-from src.handwrite.pngtosvg import PNGtoSVG
-from src.handwrite.svgtottf import SVGtoTTF
+from src.sheettopng import SHEETtoPNG
+from src.pngtosvg import PNGtoSVG
+from src.svgtottf import SVGtoTTF
 
 
 def run(sheets_dir, output_dir, characters_dir, config):
@@ -39,8 +40,33 @@ def converters(sheets_dir, output_dir=None, characters_dir=None, config=None):
     if isTempdir:
         shutil.rmtree(characters_dir)
 
+def main(data_dir_path):
+    sheets_dirs_name = os.listdir(data_dir_path)
+    sheets_dirs_name = sorted(sheets_dirs_name, key=lambda x: int(re.findall(r'\d+', x)[0]))
+
+    for sheets_dir_name in sheets_dirs_name:
+        sheets_dir = os.path.join(data_dir_path, sheets_dir_name)
+
+        try:
+            if len(sheets_dir) == 0:
+                print("You haven't chosen the sheets directory!")
+            else:
+                # characters_dir = filedialog.askdirectory(title='Choose characters directory')
+                output_parent_dir = os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)), "output"
+                )
+                if not os.path.exists(output_parent_dir):
+                    os.makedirs(output_parent_dir)
+
+                metadata = {"filename": None, "family": None, "style": None}
+                # converters(sheets_dir=sheets_dir, characters_dir=characters_dir)
+                converters(sheets_dir=sheets_dir)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
+    # run a file
     # select images' folder
     tkinter.Tk().withdraw() # prevent an empty window from appearing
     sheets_dir = filedialog.askdirectory(title='Choose sheets directory')
@@ -48,7 +74,7 @@ if __name__ == "__main__":
     if len(sheets_dir) == 0:
         print("You haven't chosen the sheets directory!")
     else:
-        # characters_dir = filedialog.askdirectory(title='Choose characters directory')
+        characters_dir = filedialog.askdirectory(title='Choose characters directory')
         output_parent_dir = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "output"
         )
@@ -58,3 +84,7 @@ if __name__ == "__main__":
         metadata = {"filename": None, "family": None, "style": None}
         # converters(sheets_dir=sheets_dir, characters_dir=characters_dir)
         converters(sheets_dir=sheets_dir)
+
+    # run multiple files
+    # data_dir_path = 'sheets'
+    # main(data_dir_path)
